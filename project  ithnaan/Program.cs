7da413cc86a627402d139c6bf026a7717231c5c6
Samuel Saunders <sam.saunders116@yahoo.com>
2017-01-14 16:13:42 -0500

    ice

--- project  ithnaan/Program.cs
+++ project  ithnaan/Program.cs
@@ -10,50 +10,165 @@ namespace project__ithnaan
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("What is your first name ");
-            string firstName = (Console.ReadLine());
-            Console.WriteLine("What is last name ");
-            string lastName = (Console.ReadLine());
-
-            Console.WriteLine("Greetings Your Excellancy" +" " + firstName + " "+ lastName);
-
-            Console.WriteLine("What is your age");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("What is your birth month");
-            int month = int.Parse(Console.ReadLine());
-            Console.WriteLine("What is your favorite ROYGBIV color,if you don't know what a ROYGBIV color is type HELP");
-            string roygbiv = (Console.ReadLine());
+            
+            bool stop = false;
 
 
-            if (roygbiv == "help")
+            while (stop==false)
             {
-                Console.WriteLine("If you do not know what ROYGBIV colors they are one of the following \n red, \n orange, \n yellow,\n green, \n blue,\n indigo, \n violet");
-                roygbiv = (Console.ReadLine());
 
+                Console.WriteLine("What is your first name ");
+                string firstName = (Console.ReadLine());
+
+                if (firstName == "quit" || firstName == "exit")
+                {
+                    break;
+                }
+                else if (firstName == "restart")
+
+                {
+                    continue;
+                }
+                Console.WriteLine("What is last name ");
+                string lastName = (Console.ReadLine());
+
+                if (lastName == "quit" || lastName == "exit")
+                {
+                    break;
+                }
+                else if (lastName == "restart")
+
+                {
+                    continue;
+                }
+
+                greetFriend(firstName, lastName);
+
+                Console.WriteLine("What is your age");
+
+                string old = Console.ReadLine();
+
+
+                if (old == "quit" || old == "exit")
+                {
+                    break;
+                }
+                else if (old == "restart")
+                {
+                    continue;
+                }
+
+                int age = int.Parse(old);
+
+                int overdaHill = OverdaHill(age);
+                Console.WriteLine(overdaHill);
+
+                Console.WriteLine("What is your numerical birth month");
+
+                string solds = Console.ReadLine();
+
+
+                if (old == "quit" || old == "exit")
+                {
+                    break;
+                }
+                else if (solds == "restart")
+
+                {
+                    continue;
+                }
+                int month = int.Parse(old);
+
+
+
+                Console.WriteLine("What is your favorite ROYGBIV color,if you don't know what a ROYGBIV color is type HELP");
+                string roygbiv = (Console.ReadLine());
+
+                if (roygbiv == "quit" || roygbiv == "exit")
+                {
+                    break;
+                }
+                else if (roygbiv == "restart")
+
+                {
+                    continue;
+                }
+                if (roygbiv == "help")
+                {
+                    Console.WriteLine("If you do not know what ROYGBIV colors they are one of the following \n red, \n orange, \n yellow,\n green, \n blue,\n indigo, \n violet");
+                    roygbiv = (Console.ReadLine());
+
+                    if (roygbiv == "quit" || roygbiv == "exit")
+                    {
+                        break;
+                    }
+                    else if (roygbiv == "restart")
+
+                    {
+                        continue;
+                    }
+                }
+                roygbiv.ToLower();
+
+
+                Console.WriteLine("How many siblings do you have");
+
+                string kids = Console.ReadLine();
+                if (kids == "quit" || kids == "exit")
+                {
+                    break;
+                }
+                else if (kids == "restart")
+
+                {
+                    continue;
+                }
+                int siblings = int.Parse(old);
+
+                double bank = 0;
+
+                double birth = yenom(month);
+                int growUp = OverdaHill(age);
+                string homes = casa( siblings);
+                string blue = rainbowBlu(roygbiv);
+
+
+     Console.WriteLine(firstName + " " + lastName + " " + "you will retire in " + " " + growUp + " " + " and have" + " " + birth + "," + homes + "and" + blue);
+
+
+                futureTells();
+                stop = true;
             }
-            roygbiv.ToLower();
-            Console.WriteLine("How many siblings do you have");
-            int siblings = int.Parse(Console.ReadLine());
+           
+
+
+        }
 
 
 
-            string retired = "";
+
+        static void greetFriend(string firstName, string lastName)
+
+        {
+
+            Console.WriteLine("Greetings your Excellancy" + " " + firstName + " " + lastName + " " + "I will tell you your fortune!");
+        }
+        static int OverdaHill(int age)
+        {
+            int retire;
             if (age % 2 == 0)
-
             {
-
-
-
-                retired = "you retire in 10 years";
-
+                retire = 30;
             }
-
             else
             {
-                retired = "you retire in 20 years";
-            }
+                retire = 1;
 
+            }
+            return retire;
+        }
+        static string casa(int siblings)
+        {
             string vacay = "";
             if (siblings == 0)
 
@@ -90,11 +205,16 @@ namespace project__ithnaan
 
                 vacay = "you have a vaction home in Tha CLEVE";
             }
-            if (siblings >= 4)
+            else if (siblings >= 4)
             {
 
                 vacay = "you have a vaction home in Iraq";
             }
+
+            return vacay;
+        }
+        static string rainbowBlu(string roygbiv)
+        {
             string ride = "";
             if (roygbiv == "red")
 
@@ -125,7 +245,7 @@ namespace project__ithnaan
             }
 
 
-            if (roygbiv == "blue")
+            else if (roygbiv == "blue")
 
             {
                 ride = "you have skates";
@@ -133,7 +253,7 @@ namespace project__ithnaan
             }
 
 
-            if (roygbiv == "indigo")
+            else if (roygbiv == "indigo")
 
             {
                 ride = " you have a plane ";
@@ -146,37 +266,45 @@ namespace project__ithnaan
             {
                 ride = " you have a spaceship ";
             }
-            string bank = "";
+            return ride;
+        }
+
+        static double yenom(int month)
+        {
+           double bank = 0;
+
             if (month < 5 && month > 0)
 
             {
-                bank = " You have $100k in the bank ";
+                bank = 100000;
             }
 
             if (month <= 8 && month >= 5)
             {
-                bank = " you have $500k in the bank ";
+                bank = 500000;
             }
-            if (month < 8 && month > 12)
+            if (month > 8 && month <= 12)
             {
-                bank = " You have $1.5 million in the bank ";
+                bank =  1500000000;
             }
             if (month <= 0)
             {
-                bank = " You will have $0 in the bank ";
+                bank = 0;
             }
+            return bank;
+        }
+        static void futureTells()
+        {
 
-
-            Console.WriteLine(firstName + " " + lastName + " " + retired + " " + bank + " " + ride + vacay);
-
+            Console.WriteLine("Your just lucky to be alive! ");
         }
     }
+
 }
 
 
 
 
 
-       
-    
+
 
